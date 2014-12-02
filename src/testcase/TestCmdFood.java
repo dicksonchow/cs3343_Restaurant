c69bf1473c8f9721b8cfe3cba7e5df12b07c5368
bosco9476 <bosco9476@Boscos-MacBook-Pro.local>
2014-12-03 04:28:19 +0800

    trying testcase in TestCmdFood

--- src/testcase/TestCmdFood.java
+++ src/testcase/TestCmdFood.java
@@ -1,6 +1,7 @@
 package testcase;
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,7 +10,18 @@ import restaurant.*;
 public class TestCmdFood {
 
 	@Before
-	public void setUp() throws Exception {}
+	public void setUp() throws Exception {
+		Food f1 = new Food("F001", "CheeseBurger", "20");
+		Food f2 = new Food("F002", "Pizza", "30");
+		Food f3 = new Food("F003", "Sushi", "20");
+		Food f4 = new Food("F004", "Sandwich", "10");
+		Food f5 = new Food("F005", "Apple Pie", "5");
+		Restaurant.getInstance().addFood(f1);
+		Restaurant.getInstance().addFood(f2);
+		Restaurant.getInstance().addFood(f3);
+		Restaurant.getInstance().addFood(f4);
+		Restaurant.getInstance().addFood(f5);
+	}
 	
 	public void tearDown() {}
 
@@ -43,6 +55,54 @@ public class TestCmdFood {
 		String[] addFood = {"F020", "CheeseBurger", "20"};
 		CmdAddFood cmd = new CmdAddFood();
 		assertEquals("Invalid input!", cmd.execute(addFood));
+	}
+	
+	@Test
+	public void testEditFoodCorrect() throws Exception{
+		String[] editFood = {"editFood", "F003", "F002", "Pizza", "30"};
+		assertEquals("Food is edited.", (new CmdEditFood()).execute(editFood));
+	}
+	
+	@Test
+	public void testEditFoodInvalidId() throws Exception{
+		String [] editFood = {"editFood", "F350", "F002", "Pizza", "30"};
+		assertEquals("Invalid food ID!", (new CmdEditFood()).execute(editFood));
+	}
+	
+	@Test
+	public void testEditFoodInvalidInput01() throws Exception{
+		String[] editFood = {"editFood", "F003", "F02", "Pizza", "30"};
+		assertEquals("Invalid input!", (new CmdEditFood()).execute(editFood));
+	}
+	
+	@Test
+	public void testEditFoodInvalidInput02() throws Exception{
+		String[] editFood = {"editFood", "F003", "F002", "", "30"};
+		assertEquals("Invalid input!", (new CmdEditFood()).execute(editFood));
+	}
+	
+	@Test
+	public void testEditFoodInvalidInput03() throws Exception{
+		String[] editFood = {"editFood", "F003", "F002", "Pizza", "-1"};
+		assertEquals("Invalid input!", (new CmdEditFood()).execute(editFood));
+	}
+	
+	@Test
+	public void testEditFoodFormatEx() throws Exception{
+		String[] editFood = {"editFood", "F003", "F002", "Pizza", "abc"};
+		assertEquals("Invalid food price!", (new CmdEditFood()).execute(editFood));
+	}
+	
+	@Test
+	public void testDeleteFoodCorrect() throws Exception{
+		String[] deleteFood = {"deleteFood", "F003"};
+		assertEquals("Food is deleted.", (new CmdDeleteFood()).execute(deleteFood));
+	}
+	
+	@Test
+	public void TestDeleteOrdertableNotValid() throws Exception{
+		String[] deleteFood = {"deleteFood", "F450"};
+		assertEquals("Invalid food ID!", (new CmdDeleteFood()).execute(deleteFood));
 	}
 
 }
